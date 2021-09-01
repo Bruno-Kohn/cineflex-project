22bf9c445888fdd90ac5a2c981e13bd0a61b52ed
Bruno Kohn <brunokohn1993@gmail.com>
2021-08-31 21:41:10 -0300

    ajustando imagem do filme hard coded

--- src/Home.js
+++ src/Home.js
@@ -6,34 +6,94 @@ export default function Home() {
       </div>
       <ul className="container">
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
         <li className="movies">
-          <div className="movie"></div>
+          <div className="movie">
+            <img
+              className="imgMovies"
+              src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+              alt=""
+            />
+          </div>
         </li>
       </ul>
     </>

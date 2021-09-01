3a2001e29f8b15911e51c9a2bc21a3aa7e2752d3
Bruno Kohn <brunokohn1993@gmail.com>
2021-08-31 22:13:11 -0300

    layout tela sessoes iniciado

---
+++ src/Session.js
@@ -0,0 +1,28 @@
+export default function Session() {
+  return (
+    <>
+      <div className="message">
+        <h2>Selecione o horário</h2>
+      </div>
+      <div className="container-movie">
+      <h1>Horario</h1>
+        <h1>Horario</h1>
+        <h1>Horario</h1>
+        <h1>Horario</h1>
+        <h1>Horario</h1>
+        <h1>Horario</h1>
+      </div>
+      <div className="container-selected-movie">
+        
+        <div className="selected-movie">
+          <img
+            className="imgMovieSelected"
+            src="https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg"
+            alt=""
+          />
+        </div>
+        <span className="movie-title">Coringa</span>
+      </div>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import ReactDOM from "react-dom";
 import Home from "./Home.js";
 import Top from "./Top.js";
+import Session from "./Session.js";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Top />
-      <Home />
-    </>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/session" exact>
+          <Session />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 

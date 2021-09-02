4ad2c23d58d69fba35807ee4d582367d99c0fea1
Bruno Kohn <brunokohn1993@gmail.com>
2021-09-02 10:26:27 -0300

    classe indisponivel

--- src/Seats.js
+++ src/Seats.js
@@ -1,14 +1,31 @@
 import { Link, useParams } from "react-router-dom";
-
+import { useEffect, useState} from "react";
+import axios from "axios";
 
 export default function Seats() {
   const { idSession } = useParams();
+  const [assentos, setAssentos] = useState([]);
   console.log(idSession);
-  const seats = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-  ];
+  // const seats = [
+  //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  //   22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+  //   41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+  // ];
+
+  useEffect(
+    () => {
+      const request = axios.get(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/showtimes/${idSession}/seats`
+      );
+
+      request.then((response) => {
+        console.log(response.data);
+        setAssentos(response.data.seats);
+      });
+    },
+    //eslint-disable-next-line
+    []
+  );
 
   return (
     <>
@@ -16,10 +33,14 @@ export default function Seats() {
         <h2>Selecione o(s) assento(s)</h2>
       </div>
       <div className="container-seats">
-        <ul className="seats ">
-          {seats.map((seat, index) => (
-            <li className="seat" key={index}>{seat}</li>
-          ))}
+        <ul className="seats">
+          {assentos === undefined
+            ? "...Loading"
+            : assentos.map((seat, index) => (
+                <li className={seat.isAvailable ? "seat" : "seat index-item-coloryellow"} key={index}>
+                  {seat.name}
+                </li>
+              ))}
         </ul>
         <div className="index">
           <div className="index-item">
